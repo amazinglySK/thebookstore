f2d85480457cea74565ceeb2db331e510a3cacea
TanmeenPatel <tanmeenpatel@gmail.com>
2023-01-13 18:32:01 +0400

    fixed design issues, added horizontal scroll ability

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,55 +1,60 @@
-
 <nav>
-    <div class="left">
-        <a href="/home">Home</a>
-        <a href="/reserve">Reserve</a>
-    </div>
-    <h1 class = "hero">The Bookstore</h1>
-    <div class="right">
-        <a href="/products">Prodcuts</a>
-        <img class = "logo" src="/dps.png" alt="">
-    </div>
+	<div class="left">
+		<a href="/home">Home</a>
+		<a href="/reserve">Reserve</a>
+	</div>
+	<h1 class="hero">The Bookstore</h1>
+	<div class="right">
+		<a href="/products">Prodcuts</a>
+		<a id="logo-link" href="https://www.dpssharjah.com" target="_blank" rel="noreferrer"
+			><img class="logo" src="/dps.png" alt="" /></a
+		>
+	</div>
 </nav>
 
 <style>
-    .hero {
-        border: 4px solid black;
-        padding: 20px;
-        font-weight: 800;
-        font-size: 2rem;
-    }
-    nav {
-        border: 4px solid black;
-        width: 90%;
-        margin: 1rem auto;
-        padding: 1.5rem 0;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        background:  url("/banner.png");
-        background-position: center;
-    }
+	.hero {
+		border: 4px solid black;
+		padding: 20px;
+		font-weight: 800;
+		font-size: 2rem;
+	}
+	nav {
+		border: 4px solid black;
+		width: 95%;
+		margin: 1.5rem auto;
+		padding: 1.5rem 0;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		background: url('/banner.png');
+		background-position: center;
+	}
 
-    a {
-        text-decoration: none;
-        border-bottom: 3px solid black;
-        font-size: 1.25rem;
-        font-weight: 600;
-        color : inherit;
-    }
+	a {
+		text-decoration: none;
+		border-bottom: 3px solid black;
+		font-size: 1.25rem;
+		font-weight: 600;
+		color: inherit;
+	}
 
+	#logo-link {
+		border-bottom: none;
+	}
 
-    .right, .left {
-        padding: 20px;
-        width: 20%;
-        display: flex;
-        align-items: center;
-        justify-content: space-around;
-    }
+	.right,
+	.left {
+		padding: 20px;
+		width: 20%;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
 
-    .logo {
-        margin-left: 40px;
-        width : 80px;
-        height: 100px;
-    }
+	.logo {
+		margin-left: 40px;
+		width: 80px;
+		height: 100px;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,72 @@
 <script>
-    import Navbar from "../components/Navbar.svelte";
-    import Section from "../components/Section.svelte";
-    import Card from "../components/Card.svelte";
+	import Navbar from '../components/Navbar.svelte';
+	import Section from '../components/Section.svelte';
+	import Card from '../components/Card.svelte';
 </script>
 
 <Navbar />
 <main>
-    <h1>Books 📖</h1>
-    <div class="container">
-        <div class="notebook-section">
-            <h2>Notebooks</h2>
-            <div class="notebooks">
-                {#each Array(5) as _, index (index)}
-                    <Card img = {"/nbk_trial.png"} item = {"Register"} status = {"IN STOCK"}/>
-                {/each}
-            </div>
-        </div>
-        <div class="textbook-section">
-            <h2>Textbooks</h2>
-            <div class="textbooks">
-                {#each Array(5) as _, index (index)}
-                    <Card img = {"/tbk_trial.png"} item = {"Physics"} status = {"IN STOCK"}/>
-                {/each}
-            </div>
-        </div>
-    </div>
-    <h1>Uniform 👔</h1>
-    <div class="container">
-        <div class="white-uniform">
-            <h2>White uniform</h2>
-            <div class="uniforms">
-                {#each Array(5) as _, index (index)}
-                    <Card img = {"/uni_trial.png"} item = {"Physics"} status = {"IN STOCK"}/>
-                {/each}
-            </div>
-        </div> 
-    </div>
+	<h1>Books 📖</h1>
+	<div class="container">
+		<div class="notebook-section">
+			<h2>Notebooks</h2>
+			<div class="notebooks">
+				{#each Array(15) as _, index (index)}
+					<Card img={'/nbk_trial.png'} item={'Register'} status={'IN STOCK'} />
+				{/each}
+			</div>
+		</div>
+		<div class="textbook-section">
+			<h2>Textbooks</h2>
+			<div class="textbooks">
+				{#each Array(11) as _, index (index)}
+					<Card img={'/tbk_trial.png'} item={'Physics'} status={'IN STOCK'} />
+				{/each}
+			</div>
+		</div>
+	</div>
+	<h1>Uniform 👔</h1>
+	<div class="container">
+		<div class="white-uniform">
+			<h2>White uniform</h2>
+			<div class="uniforms">
+				{#each Array(8) as _, index (index)}
+					<Card img={'/uni_trial.png'} item={'Physics'} status={'IN STOCK'} />
+				{/each}
+			</div>
+		</div>
+	</div>
 </main>
 
 <style>
-    main {
-        width: 90%;
-        margin: 2rem auto;
-    }
-    .notebooks, .textbooks, .uniforms{
-        display: grid;
-        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-        gap: 5rem;
-        
-    }
-    .notebook-section, .textbook-section, .white-uniform {
-        margin-left: 3rem;
-    }
-</style>    
+	main {
+		width: 95%;
+		margin: 2rem auto;
+	}
+	.notebooks,
+	.textbooks,
+	.uniforms {
+		gap: 4rem;
+		display: flex;
+		flex-wrap: nowrap;
+		overflow-x: auto;
+	}
+	.notebook-section,
+	.textbook-section,
+	.white-uniform {
+		margin-left: 3rem;
+		margin-top: 3em;
+	}
+
+	.notebooks::-webkit-scrollbar {
+		display: none;
+	}
+
+	.textbooks::-webkit-scrollbar {
+		display: none;
+	}
+
+	.uniforms::-webkit-scrollbar {
+		display: none;
+	}
+</style>
