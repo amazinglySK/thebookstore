434be3a2722feeef5b277f3f83b7bdb7b60a63bc
amazinglySK <shashwat07ktm@gmail.com>
2023-01-23 21:25:24 +0400

    Started with the cart page

---
+++ src/components/CartCard.svelte
@@ -0,0 +1,57 @@
+<script>
+    export let name
+    export let qty
+    export let price
+    export let img
+</script>
+
+
+<div class="cart-card">
+    <div class="photo">
+        <img src={img} alt="" />
+    </div>
+    <div class="content">
+        <h1>{name}</h1>
+        <div class="prod-info">
+            <span class="price">AED {price}</span>
+            <div class = "qty-group">
+                <label for = "qty">Qty</label><input type="number" name="qty" id="qty" class = "qty" bind:value={qty}>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .cart-card {
+        background-color: #C2C2C2;
+        padding: 2rem;
+        border-radius: 10px;
+		display: flex;
+        gap: 1rem;
+		margin: auto;
+	}
+
+	.content {
+		font-size: 1.25em;
+		/* background-color: antiquewhite; */
+		padding: 10px;
+        width: 100%;
+		padding-left: 30px;
+		box-sizing: border-box;
+	}
+
+    .price { 
+        font-size: 2rem;
+    }
+
+    .qty-group {
+        display: flex;
+        gap: 2rem;
+        margin-top: 10%;
+    }
+
+    .qty { 
+        height: 1rem;
+        width: 3rem;
+    }
+</style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,5 +1,49 @@
 <script>
     import Navbar from "../../components/Navbar.svelte";
+    import CartCard from "../../components/CartCard.svelte";
 </script>
 
 <Navbar />
+<main>
+    <div class="cart-container">
+        <CartCard img = {"nbk_trial.png"} name = "Register 200pgs" price = {75} qty = {1}/>
+    </div>
+    <div class="bill">
+        <h1>TOTAL</h1>
+        <p>Subtotal : AED150</p>
+        <p>Status : IN STOCK</p>
+        <button>RESERVE</button>
+    </div>
+
+</main>
+
+
+<style>
+    main {
+		width: 94%;
+		margin: 2rem auto;
+        gap: 2rem;
+        display: flex;
+	}
+    
+    .bill {
+        height: 400px;
+        width: 30%;
+        padding: 2rem;
+        display: flex;
+        flex-direction: column;
+        background-color: aliceblue;
+    }
+
+    .bill > button {
+        width: max-content;
+    }
+
+    .cart-container {
+        padding: 2rem;
+        height: max-content;
+        background-color: #E7E7E7;
+        width: 70%;
+        border-radius: 10px;
+    }
+</style>
