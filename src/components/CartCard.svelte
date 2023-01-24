075f9ff2231536c275066d8ab4d0c13364500076
TanmeenPatel <tanmeenpatel@gmail.com>
2023-01-24 13:41:46 +0400

    More work on cart page
    CO-authored : Tanmeen's Laptop

--- src/components/CartCard.svelte
+++ src/components/CartCard.svelte
@@ -1,36 +1,51 @@
 <script>
-    export let name
-    export let value
-    export let price
-    export let img
+	import { createEventDispatcher } from 'svelte';
+
+	export let name;
+	export let value;
+	export let price;
+	export let img;
+	export let idx;
+
+	const dispatch = createEventDispatcher();
+
+	function sendEvent() {
+		console.log('Event sent');
+		dispatch('remove', {
+			idx: idx
+		});
+	}
 </script>
 
-
 <div class="cart-card">
-    <div class="photo">
-        <img src={img} alt="" />
-    </div>
-    <div class="content">
-        <h1>{name}</h1>
-        <div class="prod-info">
-            <span class="price">AED {price}</span>
-            <div class = "qty-group">
-                <label for = "qty">Qty</label><input type="number" name="qty" id="qty" class = "qty" bind:value={value}>
-            </div>
-        </div>
-        <button class="remove">
-            Remove
-        </button>
-    </div>
+	<div class="photo">
+		<img src={img} alt="" />
+	</div>
+	<div class="content">
+		<h1>{name}</h1>
+		<div class="prod-info">
+			<span class="price">AED {price}</span>
+			<div class="qty-group">
+				<label for="qty">Qty</label><input
+					type="number"
+					name="qty"
+					id="qty"
+					class="qty"
+					bind:value
+				/>
+			</div>
+		</div>
+		<button class="remove" on:click={sendEvent}> Remove </button>
+	</div>
 </div>
 
 <style>
-    .cart-card {
-        background-color: #C2C2C2;
-        padding: 2rem;
-        border-radius: 10px;
+	.cart-card {
+		background-color: #c2c2c2;
+		padding: 2rem;
+		border-radius: 10px;
 		display: flex;
-        gap: 1rem;
+		gap: 1rem;
 		margin: auto;
 	}
 
@@ -38,23 +53,23 @@
 		font-size: 1.25em;
 		/* background-color: antiquewhite; */
 		padding: 10px;
-        width: 100%;
+		width: 100%;
 		padding-left: 30px;
 		box-sizing: border-box;
 	}
 
-    .price { 
-        font-size: 2rem;
-    }
+	.price {
+		font-size: 2rem;
+	}
 
-    .qty-group {
-        display: flex;
-        gap: 2rem;
-        margin-top: 10%;
-    }
+	.qty-group {
+		display: flex;
+		gap: 2rem;
+		margin-top: 10%;
+	}
 
-    .qty { 
-        height: 1rem;
-        width: 3rem;
-    }
+	.qty {
+		height: 1rem;
+		width: 3rem;
+	}
 </style>
--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,58 +1,68 @@
 <script>
-    import Navbar from "../../components/Navbar.svelte";
-    import CartCard from "../../components/CartCard.svelte";
-    import { cart } from "../stores";
+	import Navbar from '../../components/Navbar.svelte';
+	import CartCard from '../../components/CartCard.svelte';
+	import { cart } from '../stores';
 
-    let cartItems;
-    cart.subscribe(val => {
-        cartItems = val
-    })
+	let cartItems;
+	cart.subscribe((val) => {
+		cartItems = val;
+	});
 
-    $: total = cartItems.reduce((acc, curr) => acc + curr.price*curr.qty, 0)
+	function handleEvenṫ(event) {
+		$cart.splice(event.detail.idx, 1);
+	}
+
+	$: total = cartItems.reduce((acc, curr) => acc + curr.price * curr.qty, 0);
 </script>
 
 <Navbar />
 <main>
-    <div class="cart-container">
-        {#each cartItems as v, _}
-            <CartCard img = {v.img} name = {v.name} price = {v.price} qty = {v.qty}  bind:value = {v.qty}/>
-        {/each}
-    </div>
-    <div class="bill">
-        <h1>TOTAL</h1>
-        <h1>AED {total}</h1>
-        <button>RESERVE</button>
-    </div>
-
+	<div class="cart-container">
+		{#each cartItems as v, idx}
+			<CartCard
+				img={v.img}
+				name={v.name}
+				price={v.price}
+				qty={v.qty}
+				{idx}
+				bind:value={v.qty}
+				on:remove={handleEvenṫ}
+			/>
+		{/each}
+	</div>
+	<div class="bill">
+		<h1>TOTAL</h1>
+		<h1>AED {total}</h1>
+		<button>RESERVE</button>
+	</div>
 </main>
 
-
 <style>
-    main {
+	main {
 		width: 94%;
 		margin: 2rem auto;
-        gap: 2rem;
-        display: flex;
+		gap: 2rem;
+		display: flex;
 	}
-    
-    .bill {
-        height: 400px;
-        width: 30%;
-        padding: 2rem;
-        display: flex;
-        flex-direction: column;
-        background-color: aliceblue;
-    }
 
-    .bill > button {
-        width: max-content;
-    }
+	.bill {
+		height: 400px;
+		width: 30%;
+		padding: 2rem;
+		display: flex;
+		flex-direction: column;
+		background-color: aliceblue;
+	}
 
-    .cart-container {
-        padding: 2rem;
-        height: max-content;
-        background-color: #E7E7E7;
-        width: 70%;
-        border-radius: 10px;
-    }
+	.bill > button {
+		width: max-content;
+	}
+
+	.cart-container {
+		padding: 2rem;
+		height: max-content;
+		background-color: #e7e7e7;
+		width: 70%;
+		border-radius: 10px;
+	}
 </style>
